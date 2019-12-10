9156662fdec3b02324183a2f63adf12e66c80fb7
Brandon Nguyen <brandon.nguyen@kgb.com>
2019-12-10 14:35:44 -0600

    Initialize names

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
+names = ['option A', 'option B', 'option C'];
+initializeOptions(names);
+
+function createQuestion(collectionName, documentName, content) {
+    db.collection(collectionName).doc(documentName).set({
+        name: content
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    }); 
+}
+
+function initializeOptions(names) {
+    names.array.forEach(_name => {
+        db.collection("options").doc(_name).set({
+            name: _name
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        }); 
+    });
+}
 
