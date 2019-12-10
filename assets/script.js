2549be6206b3820334bbe8aa94434bd3620c07a6
Brandon Nguyen <brandon.nguyen@kgb.com>
2019-12-10 15:58:29 -0600

    populate questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
-var names = ['option A', 'option B', 'option C'];
-initializeOptions(names);
+$(function() {
+    $names = ['option A', 'option B', 'option C'];
+    initializeOptions($names);
+    $questions = getAllQuestions();
+
+    setTimeout(function() {
+        if ($questions != null) {
+            $questions.forEach(function(question) {
+                console.log(question);
+                $("#questionBody").append(
+                    "<div class='question'>" + question + "</div>"
+                );
+            });
+        }
+    }, 500);
+});
 
 function createQuestion(collectionName, documentName, content) {
     db.collection(collectionName).doc(documentName).set({
@@ -15,7 +29,7 @@ function createQuestion(collectionName, documentName, content) {
 
 function initializeOptions(names) {
     names.forEach(function(_name) {
-        db.collection("options").doc(_name).set({
+        db.collection('options').doc(_name).set({
             name: _name
         })
         .then(function() {
@@ -27,3 +41,19 @@ function initializeOptions(names) {
     });
 }
 
+function getAllQuestions() {
+    var questionsRef = db.collection('questions');
+    tempQuestions = new Array();
+
+    questionsRef.get().then(function(questionSet) {
+        questionSet.docs.map(doc => {
+            if (doc.data() != null) {
+                tempQuestions.push(doc.data().name)
+            } else {
+                console.log("No question data...");
+            }
+        });
+    });
+
+    return tempQuestions;
+}
